c953ef005114eb4416312bd4c7a2cf32c4892600
Sypth <105406568+Sypth@users.noreply.github.com>
2024-03-01 15:15:16 +0800

    :wrench: Hash

--- app/Repositories/Employee/ShowEmployeeRepository.php
+++ app/Repositories/Employee/ShowEmployeeRepository.php
@@ -5,17 +5,20 @@ namespace App\Repositories\Employee;
 use App\Repositories\BaseRepository;
 
 use App\Models\Employee\Employee;
+use Illuminate\Support\Facades\Hash;
 
 class CreateEmployeeRepository extends BaseRepository
 {
     public function execute($request)
     {
         if (Employee::where('principal_id', $request->principal_id)->first() != null) {
-            
+            $data = [];
+            $hashData = Hash::make();
         } else{
             return $this->error("You're not authorized to create school calendar enrollment");
         }
 
-        return $this->success('School calendar enrollment created successfully');
+
+        return $this->success('School calendar enrollment created successfully', $hashData);
     }
 }
