f052bd488e29fce21048827f1c15a494babf7750
melmatx <melpalana13@gmail.com>
2024-03-05 11:19:26 +0800

    Created attendance email content

---
+++ resources/views/emails/attendance.blade.php
@@ -0,0 +1,5 @@
+<h2>Good day!</h2>
+<p>This is to notify you that your child has been marked as <b>{{ $status }}</b> for today.</p>
+<p>If you have any concerns, please contact the teacher.</p>
+<p>Regards,</p>
+<p>Attend.ly</p>
