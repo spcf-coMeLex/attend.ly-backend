065417f9b113b7de76446938bd63f4f226a2670b
melmatx <melpalana13@gmail.com>
2024-03-06 11:14:15 +0800

    Modified attendance creation

--- app/Repositories/Attendance/CreateAttendanceHistoryRepository.php
+++ app/Repositories/Attendance/CreateAttendanceHistoryRepository.php
@@ -2,11 +2,9 @@
 
 namespace App\Repositories\Attendance;
 
+use App\Models\Attendance\AttendanceHistory;
 use App\Models\Section\SectionSubject;
 use App\Repositories\BaseRepository;
-
-use App\Models\Attendance\AttendanceHistory;
-
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Mail;
 
@@ -14,27 +12,95 @@ class CreateAttendanceHistoryRepository extends BaseRepository
 {
     public function execute($request)
     {
-
-        $attendance = AttendanceHistory::where('uId', $request->uId)->first();
-        if ($attendance || $request->error) {
-            return $this->error("Something went wrong!");
-        } else{
-                $sectionSubject = SectionSubject::where('code', $request->sectionSubjectCode)->first();
-                AttendanceHistory::create([
-                    "uId"                   => $request->uId,
-                    "student_id"            => $request->studentId, 
-                    "section_subject_id"    => $sectionSubject->id,
-                    "date"                  => $request->date,
-                    "time"                  => $request->time,
-                    "status"                => $request->status
-                ]);
-
-                // Mail::send('emails.attendance', ['status' => $request->status], function ($message) use ($request) {
-                //     $message->to($request->email);
-                //     $message->subject('Attendance Notification');
-                // });
+        //        $attendance = AttendanceHistory::where('uId', $request->uId)->first();
+        //        if ($attendance || $request->error) {
+        //            return $this->error("Something went wrong!");
+        //        }
+        if ($request->error) {
+            return $this->error('Something went wrong!');
         }
 
+        // Get the section subject
+        $sectionSubject = SectionSubject::where('code', $request->sectionSubjectCode)->first();
+
+        // Get the closest section subject date
+        $sectionSubjectDate = $this->getClosestSectionSubjectDate($request->date, $sectionSubject);
+
+        if (! $sectionSubjectDate) {
+            return $this->error('No schedule found for the current day!');
+        }
+
+        // Check if the student already has an attendance history for the subject at this schedule
+        $formattedTimeEnd = Carbon::parse($sectionSubjectDate->time_end)->format('H:i:s');
+        $formattedDate = Carbon::parse($request->date)->format('Y-m-d');
+
+        $attendance = AttendanceHistory::where('student_id', $request->studentId)
+            ->where('section_subject_id', $sectionSubject->id)
+            ->where('date', $formattedDate)
+            ->where('time', '<=', $formattedTimeEnd)
+            ->first();
+
+        if ($attendance) {
+            return $this->error('Attendance history already exists for this schedule!');
+        }
+
+        // Get the attendance status
+        $status = $this->getAttendanceStatus($request->time, $sectionSubjectDate);
+
+        // Create a new attendance history
+        AttendanceHistory::create([
+            'uId' => $request->uId,
+            'student_id' => $request->studentId,
+            'section_subject_id' => $sectionSubject->id,
+            'date' => $request->date,
+            'time' => $request->time,
+            'status' => $status,
+        ]);
+
+        Mail::send('emails.attendance', ['status' => $status], function ($message) use ($request) {
+            $message->to($request->email);
+            $message->subject('Attendance Notification');
+        });
+
         return $this->success('Attendance history created successfully!');
+
+    }
+
+    private function getClosestSectionSubjectDate($requestDate, $sectionSubject)
+    {
+        $parsedDate = Carbon::parse($requestDate);
+        $dayOfWeek = $parsedDate->dayName;
+        $formattedTime = $parsedDate->format('H:i');
+
+        // Get the closest section subject dates for the current day
+        $closestSectionSubjectDates = $sectionSubject->sectionSubjectDates()
+            ->where('day', strtoupper($dayOfWeek))
+            ->orderByRaw("ABS(TIMEDIFF(time_start, '$formattedTime'))")
+            ->get();
+
+        // Get the subject dates after the current time
+        $subjectDatesAfterNow = $closestSectionSubjectDates->where('time_start', '>', $formattedTime);
+
+        if ($subjectDatesAfterNow->count() > 0) {
+            return $subjectDatesAfterNow->first();
+        } else {
+            return $closestSectionSubjectDates->first();
+        }
+    }
+
+    private function getAttendanceStatus($requestTime, $sectionSubjectDate)
+    {
+        // Get the time start and time end
+        $timeStart = Carbon::parse($sectionSubjectDate->time_start);
+        $timeEnd = Carbon::parse($sectionSubjectDate->time_end);
+        $parsedTime = Carbon::parse($requestTime);
+
+        if ($parsedTime->lessThanOrEqualTo($timeStart)) {
+            return 'PRESENT';
+        } elseif ($parsedTime->lessThanOrEqualTo($timeEnd)) {
+            return 'LATE';
+        } else {
+            return 'ABSENT';
+        }
     }
 }
