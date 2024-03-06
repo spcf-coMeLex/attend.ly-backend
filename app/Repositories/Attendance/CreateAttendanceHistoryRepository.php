5e499263a48bb30cc6ba525415be684dd4e28da0
Sypth <105406568+Sypth@users.noreply.github.com>
2024-03-06 09:25:56 +0800

    :wrench: Modified the Create Attendance

--- app/Repositories/Attendance/CreateAttendanceHistoryRepository.php
+++ app/Repositories/Attendance/CreateAttendanceHistoryRepository.php
@@ -3,10 +3,12 @@
 namespace App\Repositories\Attendance;
 
 use App\Models\Section\SectionSubject;
+use App\Models\Section\SectionSubjectDate;
 use App\Repositories\BaseRepository;
 
 use App\Models\Attendance\AttendanceHistory;
-
+use App\Models\Employee\Employee;
+use App\Models\Student\Student;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Mail;
 
@@ -19,22 +21,59 @@ class CreateAttendanceHistoryRepository extends BaseRepository
         if ($attendance || $request->error) {
             return $this->error("Something went wrong!");
         } else{
-                $sectionSubject = SectionSubject::where('code', $request->sectionSubjectCode)->first();
-                AttendanceHistory::create([
-                    "uId"                   => $request->uId,
-                    "student_id"            => $request->studentId, 
-                    "section_subject_id"    => $sectionSubject->id,
-                    "date"                  => $request->date,
-                    "time"                  => $request->time,
-                    "status"                => $request->status
-                ]);
+            try {
+                //code...
+            
+                $employee = Employee::where('uId', $request->teacherUId)->first();
+                $student = Student::where('UId', $request->studentUId)->first();
+                $employeeSubjectsId = SectionSubject::where('employee_id', $employee->id)->pluck('id')->toArray();
+                $dayName = now()->format('l');
+                $now = Carbon::now();
 
+                $currentSectionSubjectDate = SectionSubjectDate::whereIn('section_subject_id', $employeeSubjectsId)
+                                        ->where('day', "=", strtoupper($dayName))
+                                        ->whereTime('time_start', '<=', $now)
+                                        ->whereTime('time_end', '>=', $now)
+                                        ->first();
+
+                if($currentSectionSubjectDate){
+                    $subjectTimeStart = Carbon::createFromFormat('H:i:s', $currentSectionSubjectDate->time_start);
+                    $timeDiff = $subjectTimeStart->diffInMinutes($now);
+                
+                    if($request->status){
+                        AttendanceHistory::create([
+                            "uId"                   => $request->uId,
+                            "student_id"            => $student->id, 
+                            "section_subject_id"    => $currentSectionSubjectDate->section_subject_id,
+                            "date"                  => $request->date,
+                            "time"                  => $request->time,
+                            "status"                => $request->status
+                        ]);
+
+                        return $this->success('Attendance history created successfully!');
+                    } else{
+                        $status = ($timeDiff >= 15) ? "Late" : "Present";
+                        AttendanceHistory::create([
+                            "uId"                   => $request->uId,
+                            "student_id"            => $student->id, 
+                            "section_subject_id"    => $currentSectionSubjectDate->section_subject_id,
+                            "date"                  => $request->date,
+                            "time"                  => $request->time,
+                            "status"                => $status
+                        ]);
+
+                        return $this->success('Attendance history created successfully!');
+                    }                
+                } else{
+                    return $this->error("There is no current section date");
+                }
                 // Mail::send('emails.attendance', ['status' => $request->status], function ($message) use ($request) {
                 //     $message->to($request->email);
                 //     $message->subject('Attendance Notification');
                 // });
+            } catch (\Exception $e) {
+                return $this->error("Error: " . $e->getMessage(), 500, [], false);
+            }
         }
-
-        return $this->success('Attendance history created successfully!');
     }
 }
